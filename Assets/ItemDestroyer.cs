9f9573e84e308618f132db97a5161e28a84d1563
原田幸生 <sloth1227@gmail.com>
2020-07-01 20:30:23 +0900

    Lesson6再提出です

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (this.transform.position.z < GameObject.Find("unitychan").transform.position.z-5) Destroy(this.gameObject);
+        
+    }
+   
+
+
+}
